b4d3245381388f20299e745a3ef3885fa59afee0
acoelho <coelho.amp@gmail.com>
2015-10-17 18:10:22 -0700

    adding r file

---
+++ proj1.R
@@ -0,0 +1,48 @@
+library(chron)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+
+if (!file.exists("./activity.csv")) {
+  if (!file.exists("./repdata-data-activity.zip")) {
+    fURL <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
+    download.file(fURL,destfile="repdata-data-activity.zip")
+  }
+  unzip("./repdata-data-activity.zip")
+}
+
+#Loading and preprocessing the data
+data <- read.csv("activity.csv")
+
+#What is mean total number of steps taken per day?
+totalStepsPerDay <- aggregate(steps ~ date, data, sum)
+hist(totalStepsPerDay$steps)
+mean(totalStepsPerDay$steps)
+median(totalStepsPerDay$steps)
+
+#What is the average daily activity pattern?
+avgStepsByInterval <- aggregate(steps ~ interval, data, mean)
+plot(avgStepsByInterval$interval, avgStepsByInterval$steps, type = "l" )
+maxStepsInterval <- avgStepsByInterval[avgStepsByInterval$steps == max(avgStepsByInterval$steps),][,"interval"]
+
+#Imputing missing values
+incompleteCases <- data[!complete.cases(data),]
+length(incompleteCases$steps)
+
+filleData <- data
+#replacing NAs with mean for that interval
+for (i in 1:length(filleData$steps)) {
+    if (is.na(filleData[i,"steps"])) {
+      filleData[i,"steps"] <- avgStepsByInterval[avgStepsByInterval$interval == filleData[i,"interval"],][,"steps"]
+    }
+    
+}
+
+totalStepsPerDayFil <- aggregate(steps ~ date, filleData, sum)
+hist(totalStepsPerDayFil$steps)
+mean(totalStepsPerDayFil$steps)
+median(totalStepsPerDayFil$steps)
+
+#Are there differences in activity patterns between weekdays and weekends?
+wkndData <- mutate(filleData, IsWeekend = gsub("FALSE", "weekday", gsub("TRUE", "weekend", is.weekend(date))))
+
